a6fc3477ae12ee70bcdf9eee73dea73d4459cf82
Joju <ejoju@deloitte.com>
2024-10-01 20:50:58 +0530

    added string_builtin file

---
+++ string_builtin.py
@@ -0,0 +1,36 @@
+#word = "chicken"
+#print(len(word))
+
+# -----------input function --------------------
+
+#num=int(input("Enter number: "))
+#print(num)
+#print(type(num))
+
+#first_name = input("Enter firstname: ")
+#last_name = input("Enter lastname: ")
+
+#print("Hi, "+first_name+" "+last_name)
+
+# -----------type casting --------------------
+#age = "19"
+
+#print(age*10)
+
+#price = 15.5
+#print(float(age))
+#print(int(price))
+
+#age = float(input("How old are you : "))
+#day = age*365
+#print(day)
+
+
+#------------f string----------------
+print(f'there are {24*60*60} seconds')
+
+
+
+
+
+
