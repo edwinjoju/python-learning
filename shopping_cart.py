93d354802560038b9860a5cb80721186a1f7ac0b
Joju <ejoju@deloitte.com>
2024-10-01 21:14:31 +0530

    added shopping cart file

---
+++ shopping_cart.py
@@ -0,0 +1,7 @@
+print("WELCOME")
+
+item = input("Enter the item: ")
+price= float(input("Enter the price of item: "))
+quantity=float(input("Enter the quantity of item: "))
+subtotal = price*quantity
+print("Subtotal="+str(subtotal))
