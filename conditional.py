47e3ce1859eade471a5c55b9765da0b53a6447fe
edwinjoju <edwinjoju15@gmail.com>
2024-10-02 10:42:39 +0530

    added conditional file

---
+++ conditional.py
@@ -0,0 +1,24 @@
+import random
+#color = input("enter color: ")
+#if(color == "green"):
+#    print("beginner")
+#elif(color == "blue"):
+#    print("intermediate!")
+#else:
+#    print("expert")
+
+
+#first_name = input("Enter first name: ")
+#last_name = input("Enter last name: ")
+
+#name_len = len(first_name)+len(last_name)
+#if(name_len>12):
+#    print(first_name+" "+last_name+" has larger length of chars")
+#elif(name_len<12):
+#    print(first_name+" "+last_name+" has lower length of chars")
+#else:
+#    print(first_name+" "+last_name+" has exact length of chars")
+
+
+print(random.randint(0,6))
+i
