706e11c3a599f2e0b89e41f525baa5db1cc232a9
Joju <ejoju@deloitte.com>
2024-10-02 17:45:03 +0530

    added for file

---
+++ for.py
@@ -0,0 +1,24 @@
+#word = "hello"
+#for x in word:
+#    print(x)
+
+#tot=0
+#for ltr in "euphoria":
+#    if ltr in "aeiou":
+#        tot+=1
+#print(tot)
+
+#for x in range(10,1,-2):
+#    print(x)
+
+n=100
+
+#while(n>1):
+#    print(f'{n} bottles of beer on the wall.\n{n} bottles of beer\nTake one down, pass it around {n-1} bottles of beer on the wall')
+#    n-=1
+
+for n in range(100,0,-1):
+    print(f'{n} bottles of beer on the wall.\n{n} bottles of beer\nTake one down, pass it around {n-1} bottles of beer on the wall')
+
+print(f'No more bottles of beer on the wall.\nNo more bottles of beer\n')
+   
