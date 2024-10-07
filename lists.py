658445c2a454758ea4df17e1a796de0195f56b97
Joju <ejoju@deloitte.com>
2024-10-07 21:05:22 +0530

    updates

--- lists.py
+++ lists.py
@@ -31,12 +31,44 @@
 #for x in lang:
 #    print(x)
 
-mark = [45,32,42,24,35,29,44,12,29]
+#mark = [45,32,42,24,35,29,44,12,29]
 
-tot=0
-for x in mark:
-    tot=tot+x
-print(tot)
-avg=tot/len(mark)
+#tot=0
+#for x in mark:
+#    tot=tot+x
+#print(tot)
+#avg=tot/len(mark)
 
-print(avg)
+#print(avg)
+
+#even=[2,4,6,8]
+#odd=[1,3,5,7,9]
+
+#print(even+odd) # add lists
+#print(even*2) # multiply lists
+
+#print(4 in even)
+#print(6 in odd)
+
+#reverse
+#marks = [32,43,22,35,16,32,18,29,40,13]
+#marks.reverse()
+#print(marks)
+
+#marks = [32,43,22,35,16,32,18,29,40,13]
+#marks.sort()
+#print(marks)
+
+#list1 = [10,20,30]
+#list2 = list1
+#print(list1==list2)
+#print(list1 is list2)
+
+#split
+#birthday = "15-08-2002"
+#print(birthday.split("-"))
+
+#join
+#letter= ["m","s","q","d"]
+#print("-".join(letter))
+
