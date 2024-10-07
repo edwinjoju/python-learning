2637a6078854ba024305fbf09142e66b4d81c09d
Edwin Joju <edwinjoju>
2024-10-07 11:50:02 +0530

    added dictionary file

---
+++ dictionary.py
@@ -0,0 +1,32 @@
+#movies = {
+#    "title":"Aavesham",
+#    "review":12046,
+#    "imdb":8.7,
+#}
+
+#print(movies["title"])
+
+price = {
+    "apple":50,
+    "mango":40,
+    "cherry":70,
+    "jackfruit":35,
+    "banana":25,
+    "strawberry":70,
+}
+
+#get method
+#product = input("enter product: ")
+#price = price.get(product)
+#if price:
+#    print(f'{product} is ${price}')
+#else:
+#    print("could not find product...")
+
+popval= price.pop('apple')
+print(popval)
+
+print(price)
+
+popval = price.popitem()
+print(price)
