b9fb929141039b9626b6729ad47fdd50e1c618fe
Joju <ejoju@deloitte.com>
2024-10-10 21:37:46 +0530

    added dictionary file

--- dictionary.py
+++ dictionary.py
@@ -6,14 +6,27 @@
 
 #print(movies["title"])
 
-price = {
-    "apple":50,
-    "mango":40,
-    "cherry":70,
-    "jackfruit":35,
-    "banana":25,
-    "strawberry":70,
-}
+
+
+#max_price = 0
+#high_qual = ""
+
+#for x,y in price.items():
+#    print(y, max_price)
+#    if(y > max_price):
+ #       max_price=y
+#        high_qual=x
+
+#print(max_price, high_qual)
+
+#prints out keys
+#for x in price.keys():
+#    print(x)
+#prints out key-value pairs
+#for x in price.items():
+#    print(x)
+#for x in price.values():
+#    print(x)
 
 #get method
 #product = input("enter product: ")
@@ -23,10 +36,33 @@ price = {
 #else:
 #    print("could not find product...")
 
-popval= price.pop('apple')
-print(popval)
+#popval= price.pop('apple')
+#print(popval)
 
-print(price)
+#print(price)
 
-popval = price.popitem()
-print(price)
+#popval = price.popitem()
+#print(price)
+
+
+price = {
+    "apple":50,
+    "mango":40,
+    "cherry":75,
+    "jackfruit":35,
+    "banana":25,
+    "strawberry":70,
+}
+
+vegetable = {
+    "carrot":10,
+    "tomato":12,
+    "brinjal":20,
+    "peas":10
+}
+
+#price.update({"grape":45})
+#print(price)
+
+#food = vegetable | price
+#print(food)
