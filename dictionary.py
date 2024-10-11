1fbb4deb5342a799d8122d8c828cb3983c1b50d7
Edwin Joju <edwinjoju>
2024-10-11 09:06:44 +0530

    updated dictionary

--- dictionary.py
+++ dictionary.py
@@ -6,27 +6,14 @@
 
 #print(movies["title"])
 
-
-
-#max_price = 0
-#high_qual = ""
-
-#for x,y in price.items():
-#    print(y, max_price)
-#    if(y > max_price):
- #       max_price=y
-#        high_qual=x
-
-#print(max_price, high_qual)
-
-#prints out keys
-#for x in price.keys():
-#    print(x)
-#prints out key-value pairs
-#for x in price.items():
-#    print(x)
-#for x in price.values():
-#    print(x)
+price = {
+    "apple":50,
+    "mango":40,
+    "cherry":70,
+    "jackfruit":35,
+    "banana":25,
+    "strawberry":70,
+}
 
 #get method
 #product = input("enter product: ")
@@ -38,31 +25,9 @@
 
 #popval= price.pop('apple')
 #print(popval)
-
 #print(price)
 
 #popval = price.popitem()
 #print(price)
 
 
-price = {
-    "apple":50,
-    "mango":40,
-    "cherry":75,
-    "jackfruit":35,
-    "banana":25,
-    "strawberry":70,
-}
-
-vegetable = {
-    "carrot":10,
-    "tomato":12,
-    "brinjal":20,
-    "peas":10
-}
-
-#price.update({"grape":45})
-#print(price)
-
-#food = vegetable | price
-#print(food)
