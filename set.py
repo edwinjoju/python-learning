8c100ee550070d453802f6f3170622aa734f767a
Edwin Joju <edwinjoju>
2024-10-12 15:37:54 +0530

    added set file

--- set.py
+++ set.py
@@ -1,2 +1,17 @@
-set_val = {10,20,30,40}
-print(set_val)
+#set_val = {10,20,30,40}
+#print(set_val)
+#set_val.add(50)
+#print(set_val)
+#print(len(set_val))
+#set_val.remove(40)
+#print(set_val)
+#set_val.clear()
+#print(set_val)
+
+
+#intersection
+even ={2,4,6,8,10}
+prime = {2,3,5,7,9}
+print(even & prime)
+print(even | prime)
+print(even-prime)
