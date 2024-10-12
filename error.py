38d90aa957e940464b2df29f840d227377373618
Edwin Joju <edwinjoju>
2024-10-12 23:43:31 +0530

    error

---
+++ error.py
@@ -0,0 +1,15 @@
+#ERRORS
+#syntaxError
+#NameError
+#IndexError
+#TypeError
+#ValueError
+#KeyError
+
+
+#raise Exception("Fuck you bitch")
+
+try:
+    num = int(input("enter your age: "))
+except ValueError:
+    print("age is a number you idiot")
