c837b25ab2907d05e0790575064681cd3e61967d
Edwin Joju <edwinjoju>
2024-10-13 11:58:15 +0530

    added modules, utils, app

--- error.py
+++ error.py
@@ -9,7 +9,10 @@
 
 #raise Exception("Fuck you bitch")
 
-try:
-    num = int(input("enter your age: "))
-except ValueError:
-    print("age is a number you idiot")
+#try:
+#    num = int(input("enter your age: "))
+#except ValueError:
+#    print("age is a number you idiot")
+
+#LBYL : Look Before You Leap
+#EAFP : Easier to Ask for Forgiveness than Permission
---
+++ modules.py
@@ -0,0 +1,13 @@
+#import random
+#print(random.randint(1,6))
+#import calendar
+#year=int(input("enter year: "))
+#print(calendar.isleap(year))
+#print(calendar.weekday(2002,8,15))
+#import math
+#print(math.cos(1))
+#import random as rand
+#print(rand.randint(1,12))
+#from math import pi, sin
+
+
